6234898409fecdf2e577a2b37f0e331deecc3362
EmersonLaranja <emersonlaranja@gmail.com>
2023-10-04 18:18:13 -0300

    video: aplicando o TipoPet no CRUD

--- src/controller/PetController.ts
+++ src/controller/PetController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import type TipoPet from "../tipos/TipoPet";
-const listaDePets = [];
+const listaDePets: TipoPet[] = [];
 export default class PetController {
   criaPet(req: Request, res: Response) {
     const { id, especie, idade, nome } = <TipoPet>req.body;
@@ -21,6 +21,34 @@ export default class PetController {
       nome,
     };
     listaDePets.push(novoPet);
-    return res.json(novoPet);
+    return res.status(200).json(novoPet);
+  }
+  listaPets(req: Request, res: Response) {
+    return res.status(200).json(listaDePets);
+  }
+  atualizaPet(req: Request, res: Response) {
+    const { id } = req.params;
+    const { especie, idade, nome } = req.body as TipoPet;
+    const pet = listaDePets.find((pet) => pet.id === Number(id));
+
+    if (!pet) {
+      return res.status(400).json({ mensagem: "Pet não encontrado" });
+    }
+    pet.nome = nome;
+    pet.idade = idade;
+    pet.especie = especie;
+    return res.status(200).json(pet);
+  }
+
+  deletaPet(req: Request, res: Response) {
+    const { id } = req.params;
+    const pet = listaDePets.find((pet) => pet.id === Number(id));
+    if (!pet) {
+      return res.status(400).json({ mensagem: "Pet não encontrado" });
+    }
+
+    const indice = listaDePets.indexOf(pet);
+    listaDePets.splice(indice, 1);
+    return res.status(204).json();
   }
 }
--- src/routes/petRoutes.ts
+++ src/routes/petRoutes.ts
@@ -6,5 +6,14 @@ const petController = new PetController();
 router.post("/", (req, res) => {
   petController.criaPet(req, res);
 });
+router.get("/", (req, res) => {
+  petController.listaPets(req, res);
+});
+router.put("/:id", (req, res) => {
+  petController.atualizaPet(req, res);
+});
+router.delete("/:id", (req, res) => {
+  petController.deletaPet(req, res);
+});
 
 export default router;

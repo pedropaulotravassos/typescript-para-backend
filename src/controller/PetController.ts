aab6fa46d029414ffd6e29cb824590f222ecd406
EmersonLaranja <emersonlaranja@gmail.com>
2023-10-04 18:54:54 -0300

    video: operador in na validacao

--- src/controller/PetController.ts
+++ src/controller/PetController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import type TipoPet from "../tipos/TipoPet";
 import EnumEspecie from "../enum/EnumEspecie";
+import EnumPorte from "../enum/EnumPorte";
 import PetRepository from "../repositories/PetRepository";
 import PetEntity from "../entities/PetEntity";
 let listaDePets: Array<TipoPet> = [];
@@ -14,13 +15,24 @@ function geraId() {
 export default class PetController {
   constructor(private repository: PetRepository) {}
   async criaPet(req: Request, res: Response) {
-    const { adotado, especie, dataDeNascimento, nome } = <PetEntity>req.body;
+    const { adotado, especie, dataDeNascimento, nome, porte } = <PetEntity>(
+      req.body
+    );
 
     if (!Object.values(EnumEspecie).includes(especie)) {
       return res.status(400).json({ error: "Especie inválida" });
     }
 
-    const novoPet = new PetEntity(nome, especie, dataDeNascimento, adotado);
+    if (porte && !(porte in EnumPorte)) {
+      return res.status(400).json({ error: "Porte inválido" });
+    }
+    const novoPet = new PetEntity(
+      nome,
+      especie,
+      dataDeNascimento,
+      adotado,
+      porte
+    );
 
     await this.repository.criaPet(novoPet);
     return res.status(201).json(novoPet);

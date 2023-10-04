70463e6431aaa22d75e60baee97bfd813fd8faa4
EmersonLaranja <emersonlaranja@gmail.com>
2023-10-04 18:45:32 -0300

    video: inicializando um campo fora do construtor

--- src/controller/PetController.ts
+++ src/controller/PetController.ts
@@ -13,20 +13,16 @@ function geraId() {
 
 export default class PetController {
   constructor(private repository: PetRepository) {}
-  criaPet(req: Request, res: Response) {
+  async criaPet(req: Request, res: Response) {
     const { adotado, especie, dataDeNascimento, nome } = <PetEntity>req.body;
 
     if (!Object.values(EnumEspecie).includes(especie)) {
       return res.status(400).json({ error: "Especie inválida" });
     }
 
-    const novoPet = new PetEntity();
-    (novoPet.id = geraId()),
-      (novoPet.adotado = adotado),
-      (novoPet.especie = especie),
-      (novoPet.dataDeNascimento = dataDeNascimento),
-      (novoPet.nome = nome),
-      this.repository.criaPet(novoPet);
+    const novoPet = new PetEntity(nome, especie, dataDeNascimento, adotado);
+
+    await this.repository.criaPet(novoPet);
     return res.status(201).json(novoPet);
   }
 

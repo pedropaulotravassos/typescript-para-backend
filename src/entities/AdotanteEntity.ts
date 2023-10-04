0f59fbd072784eb99425e6f63d0de97b4922de0a
EmersonLaranja <emersonlaranja@gmail.com>
2023-10-04 18:51:10 -0300

    video: criando a entidade Endereco

---
+++ src/entities/AdotanteEntity.ts
@@ -0,0 +1,44 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import EnderecoEntity from "./Endereco";
+
+@Entity()
+export default class AdotanteEntity {
+  @PrimaryGeneratedColumn()
+  id!: number;
+  @Column()
+  nome: string;
+  @Column()
+  senha: string;
+  @Column()
+  celular: string;
+  @Column({ nullable: true })
+  foto?: string;
+
+  @OneToOne(() => EnderecoEntity, {
+    nullable: true,
+    cascade: true,
+    eager: true,
+  })
+  @JoinColumn()
+  endereco?: EnderecoEntity;
+
+  constructor(
+    nome: string,
+    senha: string,
+    celular: string,
+    foto?: string,
+    endereco?: EnderecoEntity
+  ) {
+    this.nome = nome;
+    this.senha = senha;
+    this.foto = foto;
+    this.celular = celular;
+    this.endereco = endereco;
+  }
+}
---
+++ src/repositories/interfaces/InterfaceAdotanteRepository.ts
@@ -0,0 +1,16 @@
+import AdotanteEntity from "../../entities/AdotanteEntity";
+
+export default interface InterfaceAdotanteRepository {
+  criaAdotante(adotante: AdotanteEntity): void | Promise<void>;
+
+  listaAdotantes(): AdotanteEntity[] | Promise<AdotanteEntity[]>;
+
+  atualizaAdotante(
+    id: number,
+    adotante: AdotanteEntity
+  ): Promise<{ success: boolean; message?: string }> | void;
+
+  deletaAdotante(
+    id: number
+  ): Promise<{ success: boolean; message?: string }> | void;
+}

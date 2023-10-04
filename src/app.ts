5fc2215f09c244e5c3a7de44829cc3b9b0a0985f
EmersonLaranja <emersonlaranja@gmail.com>
2023-10-04 18:12:16 -0300

    video: entendendo a inferência de tipos

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,27 @@
-import express, { Request, Response } from "express";
+import express, { Response } from "express";
+import router from "./routes";
 
 const app = express();
 app.use(express.json());
+router(app);
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", (_, res: Response) => {
   res.send("Bem vindo ao curso de TypeScript!");
 });
 
-function criaPet(id: number, nome: string, especie: string, idade: number) {
+function criaPet(
+  id: number,
+  nome: string,
+  especie: string,
+  idade: number,
+  adotado: boolean
+) {
   return {
     id,
     nome,
     especie,
     idade,
+    adotado,
   };
 }
 
@@ -22,9 +31,9 @@ function geraId() {
   return id;
 }
 
-app.post("/pets", (req: Request, res: Response) => {
-  const pet1 = criaPet(geraId(), "Bolt", "cachorro", 3);
-  const pet2 = criaPet(geraId(), "Mel", "gato", 2);
+app.post("/pets", (_, res) => {
+  const pet1 = criaPet(geraId(), "Bolt", "cachorro", 3, false);
+  const pet2 = criaPet(geraId(), "Mel", "gato", 2, false);
 
   res.send([pet1, pet2]);
 });
